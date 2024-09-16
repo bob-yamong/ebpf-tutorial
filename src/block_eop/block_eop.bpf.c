5e27ffd520dce26e24aa482ef685e78c51c8930f
Changhyun Lee <dlckdgus200011@gmail.com>
2024-09-17 02:36:58 +0900

    Feat: can specify pid to block eop
    get_container_pid func is needed to specify container by name

--- src/block_eop/block_eop.bpf.c
+++ src/block_eop/block_eop.bpf.c
@@ -1,54 +1,46 @@
 #include <vmlinux.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
 
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __type(key, __u32);   // pid
     __type(value, __u32);
     __uint(max_entries, 1024);
-} pid_map SEC(".maps");
+} ns_id_map SEC(".maps");
 
-// 이건 그냥 자격 증명 때 호출되는 lsm hook 이었음
-// SEC("lsm/cred_prepare")
-// int BPF_PROG(prevent_root, struct cred *new, const struct cred *old, gfp_t gfp) {
-//     __u32 pid = bpf_get_current_pid_tgid() >> 32;
-//     __u32 *watched = bpf_map_lookup_elem(&pid_map, &pid);
-
-//     bpf_printk("lsm/cred_prepare for pid %d, new uid: %d, old uid: %d", pid, new->uid.val, old->uid.val);
-
-//     if (watched && new->uid.val == 0) {
-//         bpf_printk("Prevented root escalation for pid %d\n", pid);
-//         return -1;
-//     }
-
-//     if (new->uid.val == 0 && old->uid.val != 0) {
-//         bpf_printk("Prevented root escalation for pid %d\n", pid);
-//         return -1;
-//     }
-
-//     return 0;
-// }
+char LICENSE[] SEC("license") = "GPL";
 
 SEC("lsm/task_fix_setuid")
 int BPF_PROG(prevent_root_setuid, struct cred *new, const struct cred *old, int flags) {
-    __u32 pid = bpf_get_current_pid_tgid() >> 32;
-    __u32 *watched = bpf_map_lookup_elem(&pid_map, &pid);
+    struct task_struct *task;
+    struct nsproxy *nsproxy;
+    struct pid_namespace *pid_ns;
+    __u32 ns_id;
+    __u32 pid;
+    __u32 new_uid, old_uid;
 
-    bpf_printk("lsm/task_fix_setuid for pid %d, new uid: %d, old uid: %d", 
-               pid, new->uid.val, old->uid.val);
+    task = (struct task_struct *)bpf_get_current_task();
 
-    if (watched && new->uid.val == 0) {
-        bpf_printk("Prevented root escalation for watched pid %d", pid);
+    // bpf_core_read(&dst, size, src)에서 dst는 데이터를 저장할 변수, size는 읽을 데이터의 크기, src는 읽을 커널 구조체의 필드
+    bpf_core_read(&nsproxy, sizeof(nsproxy), &task->nsproxy);
+    bpf_core_read(&pid_ns, sizeof(pid_ns), &nsproxy->pid_ns_for_children);
+    bpf_core_read(&ns_id, sizeof(ns_id), &pid_ns->ns.inum);
+
+    pid = bpf_get_current_pid_tgid() >> 32;
+    bpf_core_read(&new_uid, sizeof(new_uid), &new->uid.val);
+    bpf_core_read(&old_uid, sizeof(old_uid), &old->uid.val);
+
+    __u32 *watched = bpf_map_lookup_elem(&ns_id_map, &ns_id);
+
+    bpf_printk("lsm/task_fix_setuid for ns_id %u, pid %u, new uid: %u, old uid: %u", 
+               ns_id, pid, new_uid, old_uid);
+
+    if (watched && new_uid == 0) {
+        bpf_printk("Prevented root escalation for watched ns_id %u", ns_id);
         return -1;
     }
 
-    // if (new->uid.val == 0 && old->uid.val != 0) {
-    //     bpf_printk("Prevented global root escalation for pid %d", pid);
-    //     return -1;
-    // }
-
     return 0;
 }
-
-char LICENSE[] SEC("license") = "GPL";
--- src/block_eop/block_eop.c
+++ src/block_eop/block_eop.c
@@ -2,9 +2,47 @@
 #include <unistd.h>
 #include <signal.h>
 #include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
 #include <linux/types.h>
 #include <bpf/libbpf.h>
 #include "block_eop.skel.h"
+
+#define MAX_PATH 256
+
+int get_namespace_id(int container_pid) {
+    
+    // PID 네임스페이스 ID 찾기
+    char path[MAX_PATH];
+    snprintf(path, sizeof(path), "/proc/%d/ns/pid", container_pid);
+    
+    int fd = open(path, O_RDONLY);
+    if (fd < 0) {
+        perror("Failed to open namespace file");
+        return 1;
+    }
+
+    char link_target[MAX_PATH];
+    ssize_t len = readlink(path, link_target, sizeof(link_target)-1);
+    if (len < 0) {
+        perror("Failed to read link");
+        close(fd);
+        return 1;
+    }
+    link_target[len] = '\0';
+
+    unsigned int ns_id;
+    if (sscanf(link_target, "pid:[%u]", &ns_id) != 1) {
+        fprintf(stderr, "Failed to parse namespace ID\n");
+        close(fd);
+        return 1;
+    }
+
+    printf("PID namespace ID for PID %d: %u\n", container_pid, ns_id);
+
+    close(fd);
+    return ns_id;
+}
 
 int main(int argc, char **argv) {
     struct block_eop_bpf *skel;
@@ -33,7 +71,8 @@ int main(int argc, char **argv) {
     while (1) {
         char pid_str[256];
         __u32 pid;
-        __u32 value = 1;
+        __u32 value;    // 무의미한 값
+        __u32 ns_id;
 
         printf("Enter pid name to restrict (or 'quit' to exit): ");
         if (fgets(pid_str, sizeof(pid_str), stdin) == NULL) {
@@ -47,9 +86,10 @@ int main(int argc, char **argv) {
         }
 
         pid = (__u32)atoi(pid_str);
+        ns_id = get_namespace_id(pid);
 
         // update eBPF map
-        err = bpf_map__update_elem(skel->maps.pid_map, &pid, sizeof(pid), &value, sizeof(value), BPF_ANY);
+        err = bpf_map__update_elem(skel->maps.ns_id_map, &ns_id, sizeof(ns_id), &value, sizeof(value), BPF_ANY);
         if (err) {
             fprintf(stderr, "Failed to update map: %d\n", err);
             continue;

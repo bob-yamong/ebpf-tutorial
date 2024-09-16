0cc894fca0c4447e3605f340bb457eae0afb87d8
Changhyun Lee <dlckdgus200011@gmail.com>
2024-09-17 03:19:59 +0900

    Feat: Simplify the code

--- src/block_eop/block_eop.bpf.c
+++ src/block_eop/block_eop.bpf.c
@@ -14,19 +14,12 @@ char LICENSE[] SEC("license") = "GPL";
 
 SEC("lsm/task_fix_setuid")
 int BPF_PROG(prevent_root_setuid, struct cred *new, const struct cred *old, int flags) {
-    struct task_struct *task;
-    struct nsproxy *nsproxy;
-    struct pid_namespace *pid_ns;
     __u32 ns_id;
     __u32 pid;
     __u32 new_uid, old_uid;
 
-    task = (struct task_struct *)bpf_get_current_task();
-
-    // bpf_core_read(&dst, size, src)에서 dst는 데이터를 저장할 변수, size는 읽을 데이터의 크기, src는 읽을 커널 구조체의 필드
-    bpf_core_read(&nsproxy, sizeof(nsproxy), &task->nsproxy);
-    bpf_core_read(&pid_ns, sizeof(pid_ns), &nsproxy->pid_ns_for_children);
-    bpf_core_read(&ns_id, sizeof(ns_id), &pid_ns->ns.inum);
+    struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+    ns_id = BPF_CORE_READ(task, nsproxy, pid_ns_for_children, ns.inum);
 
     pid = bpf_get_current_pid_tgid() >> 32;
     bpf_core_read(&new_uid, sizeof(new_uid), &new->uid.val);
@@ -34,8 +27,7 @@ int BPF_PROG(prevent_root_setuid, struct cred *new, const struct cred *old, int 
 
     __u32 *watched = bpf_map_lookup_elem(&ns_id_map, &ns_id);
 
-    bpf_printk("lsm/task_fix_setuid for ns_id %u, pid %u, new uid: %u, old uid: %u", 
-               ns_id, pid, new_uid, old_uid);
+    bpf_printk("lsm/task_fix_setuid for ns_id %u, pid %u, new uid: %u, old uid: %u", ns_id, pid, new_uid, old_uid);
 
     if (watched && new_uid == 0) {
         bpf_printk("Prevented root escalation for watched ns_id %u", ns_id);
@@ -44,3 +36,11 @@ int BPF_PROG(prevent_root_setuid, struct cred *new, const struct cred *old, int 
 
     return 0;
 }
+// bpf_core_read(&dst, size, src)에서 dst는 데이터를 저장할 변수, size는 읽을 데이터의 크기, src는 읽을 커널 구조체의 필드
+
+// struct task_struct *task;
+// struct nsproxy *nsproxy;
+// struct pid_namespace *pid_ns;
+// bpf_core_read(&nsproxy, sizeof(nsproxy), &task->nsproxy);
+// bpf_core_read(&pid_ns, sizeof(pid_ns), &nsproxy->pid_ns_for_children);
+// bpf_core_read(&ns_id, sizeof(ns_id), &pid_ns->ns.inum);
